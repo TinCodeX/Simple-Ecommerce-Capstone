da105b6468e11f8e31500618a58bc8f3ee4b33fd
Tinsae Befekadu <tinsaebefekadu2012@gmail.com>
2026-03-01 20:30:14 +0300

    FIX: db url and env config

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -13,18 +13,31 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 import dj_database_url
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-import os
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
+if os.environ.get("DATABASE_URL"):  # Production
+    DATABASES = {
+        "default": dj_database_url.config(default=os.environ.get("DATABASE_URL"))
+    }
+else:  # Local development
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 DEBUG = False
 
 ALLOWED_HOSTS = ['.onrender.com']
